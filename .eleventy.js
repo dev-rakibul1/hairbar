075b7d542c0b2b9d555e2009c104e576f9252b2d
Your Name <you@example.com>
2023-05-10 00:44:43 +0600

    elevendds

--- .eleventy.js
+++ .eleventy.js
@@ -28,8 +28,8 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      input: "/src",
-      output: "/public",
+      input: "src",
+      output: "public",
     },
   };
 };
